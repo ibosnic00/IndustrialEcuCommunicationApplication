dcfba05646bf357cf14d349b642f1017aa460e06
Ivan Bosnic <ivan.bosnic.st@gmail.com>
2021-01-24 19:30:24 +0100

    Removed obsolete Event Handler OnMessageReceived and added REQUEST_PGN case in event filters

--- IndustrialEcuCommunicationApplication/Program.cs
+++ IndustrialEcuCommunicationApplication/Program.cs
@@ -43,9 +43,8 @@ namespace IECA
 
         #region Fields
 
-        Queue<KeyValuePair<uint, List<byte>>> requestQue = new Queue<KeyValuePair<uint, List<byte>>>();
         J1939ToStringConverter? J1939ToJsonConverter;
-        List<MultiFrameMessage>? mfMsgsBuffer;
+        List<MultiFrameMessage>? MfMsgsBuffer;
 
         #endregion Fields
 
@@ -61,7 +60,7 @@ namespace IECA
             program.J1939ToJsonConverter = new J1939ToStringConverter(configuration);
             Console.WriteLine($"Configuration file has {configuration.Count} defined PGN's");
 
-            program.mfMsgsBuffer = new List<MultiFrameMessage>();
+            program.MfMsgsBuffer = new List<MultiFrameMessage>();
             program.MultiFrameMessageReceived += program.OnMultiFrameMessageReceived;
 
             var canInterface = new SocketCanInterface(selectedChannel);
@@ -74,9 +73,9 @@ namespace IECA
                 // if any multiframe message is fully received
                 _ = Task.Run(() =>
                 {
-                    if (program.mfMsgsBuffer.Any(msg => msg.IsMessageComplete == true))
+                    if (program.MfMsgsBuffer.Any(msg => msg.IsMessageComplete == true))
                     {
-                        var receivedMFMessage = program.mfMsgsBuffer.SingleOrDefault(msg => msg.IsMessageComplete == true);
+                        var receivedMFMessage = program.MfMsgsBuffer.SingleOrDefault(msg => msg.IsMessageComplete == true);
                         program.MultiFrameMessageReceived?.Invoke(program, receivedMFMessage);
                     }
                 });
@@ -126,14 +125,18 @@ namespace IECA
             }
             else if (receivedPdu.ParameterGroupNumber == TP_DT_PGN)
             {
-                if (mfMsgsBuffer == null || msg.DLC < TP_CM_DATA_SIZE)
+                if (MfMsgsBuffer == null || msg.DLC < TP_DT_DATA_SIZE)
                     return;
 
-                if (mfMsgsBuffer.Exists(msg => msg.PDU.SourceAddress == receivedPdu.SourceAddress))
+                if (MfMsgsBuffer.Exists(msg => msg.PDU.SourceAddress == receivedPdu.SourceAddress))
                 {
                     var dataTransferMessage = new DataTransferMessage(msg.Data.ToList(), receivedPdu.Specific.Value, receivedPdu.SourceAddress);
                     HandleReceivedDataTransferMessage(dataTransferMessage);
                 }
+            }
+            else if (receivedPdu.ParameterGroupNumber == REQUEST_PGN)
+            {
+                //currently not in use
             }
             else if (receivedPdu.ParameterGroupNumber == REQUEST_2_PGN)
             {
@@ -150,66 +153,6 @@ namespace IECA
             }
         }
 
-        private void OnMessageReceived(object? sender, CanMessage msg)
-        {
-            if (!msg.IsExtendedId)
-                return;
-
-            var receivedPdu = ProtocolDataUnit.FromCanExtIdentifierFormat(msg.ID);
-
-            var receivedMessage = new J1939Message(receivedPdu, msg.Data.ToList());
-            if (J1939ToJsonConverter != null)
-                Console.WriteLine(J1939ToJsonConverter.ConvertJ1939MessageToHumanReadableFormat(receivedMessage));
-
-            // specific request
-            if (receivedPdu.ParameterGroupNumber == REQUEST_PGN && receivedPdu.Specific.Value != GLOBAL_DESTINATION_ADDRESS)
-            {
-                //TODO: key should be 4 byte ID - specific request
-                requestQue.Enqueue(new KeyValuePair<uint, List<byte>>(receivedPdu.SourceAddress, receivedMessage.Data));
-            }
-
-            // global request
-            if (receivedPdu.ParameterGroupNumber == REQUEST_PGN && receivedPdu.Specific.Value == GLOBAL_DESTINATION_ADDRESS)
-            {
-                //TODO: key should be 4 byte ID - global request
-                requestQue.Enqueue(new KeyValuePair<uint, List<byte>>(receivedPdu.SourceAddress, receivedMessage.Data));
-            }
-
-            // PDU1 Format
-            if (receivedPdu.Specific is DestinationAddress destinationAddress)
-            {
-                if (destinationAddress.Value == GLOBAL_DESTINATION_ADDRESS)
-                {
-                    // USE JUMP TABLE FOR PGN VALUES OF INTEREST AND
-                    // IF SA = ID OF SPECIAL INTEREST
-                    // THEN
-                    // SAVE 8 BYTES OF DATA IN DEDICATED BUFFER
-                    // ELSE
-                    // SAVE 12 BYTE MESSAGE(ID AND DATA) IN CIRCULAR QUE
-                }
-                else
-                {
-                    // USE JUMP TABLE FOR PGN VALUES OF INTEREST AND
-                    // IF SA = ID OF SPECIAL INTEREST VALUES
-                    // THEN
-                    // SAVE 8 BYTES OF DATA IN DEDICATED BUFFER
-                    // ELSE
-                    // SAVE 12 BYTE MESSAGE(ID AND DATA) IN CIRCULAR QUE
-                }
-            }
-
-            //PDU2 Format
-            if (receivedPdu.Specific is GroupExtension groupExtension)
-            {
-                // USE JUMP TABLE FOR PGN VALUES OF INTEREST AND
-                // IF SA = ID OF SPECIAL INTEREST
-                // THEN
-                // SAVE 8 BYTES OF DATA IN DEDICATED BUFFER
-                // ELSE
-                // SAVE 12 BYTE MESSAGE (ID AND DATA) IN CIRCULAR QUE
-            }
-        }
-
         #endregion Event Handlers
 
 
@@ -219,25 +162,25 @@ namespace IECA
         private void HandleReceivedBroadcastAnnounceMessage(BrodcastAnnounceMessage rcvMsg)
         {
             // remove any message that in buffer with same source - only one PGN per transfer is allowed
-            mfMsgsBuffer?.RemoveAll(msg => msg.PDU.SourceAddress == rcvMsg.PDU.SourceAddress);
+            MfMsgsBuffer?.RemoveAll(msg => msg.PDU.SourceAddress == rcvMsg.PDU.SourceAddress);
             // add new mf message in buffer
-            mfMsgsBuffer?.Add(new MultiFrameMessage(rcvMsg.Pgn, rcvMsg.PDU.SourceAddress));
+            MfMsgsBuffer?.Add(new MultiFrameMessage(rcvMsg.Pgn, rcvMsg.PDU.SourceAddress));
         }
 
         private void HandleReceivedDataTransferMessage(DataTransferMessage rcvMsg)
         {
             //TODO: Erase after testing
             Console.WriteLine("Received data transfer message for pgn " + rcvMsg.PDU.ParameterGroupNumber + " sqNo: " + rcvMsg.SequenceNumber);
-            mfMsgsBuffer.Single(msg => msg.PDU.SourceAddress == rcvMsg.PDU.SourceAddress).AddPacketizedData(rcvMsg.PacketizedData);
+            MfMsgsBuffer.Single(msg => msg.PDU.SourceAddress == rcvMsg.PDU.SourceAddress).AddPacketizedData(rcvMsg.PacketizedData);
         }
 
         private void RemoveCompletedMultiFrameMessageFromBuffer(MultiFrameMessage rcvMsg)
         {
-            if (mfMsgsBuffer == null)
+            if (MfMsgsBuffer == null)
                 return;
 
             //TODO: Check will this erase correct msg
-            mfMsgsBuffer.RemoveAll(mfMsg => mfMsg.PDU.SourceAddress == rcvMsg.PDU.SourceAddress
+            MfMsgsBuffer.RemoveAll(mfMsg => mfMsg.PDU.SourceAddress == rcvMsg.PDU.SourceAddress
                                                                 && mfMsg.PDU.Specific.Value == rcvMsg.PDU.Specific.Value);
         }
 
