82a50e18dae4fcc5e9f972decb4ff0db6df58fc8
Ivan Bosnic <ivan.bosnic.st@gmail.com>
2021-01-28 16:05:20 +0100

    Extracted all configurable parameters to json configuration file and added logic for parsing

--- IndustrialEcuCommunicationApplication/J1939/Configuration/DataConfigurationDeserializer.cs
+++ IndustrialEcuCommunicationApplication/J1939/Configuration/DataConfigurationDeserializer.cs
@@ -4,14 +4,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
-namespace IECA.J1939
+namespace IECA.J1939.Configuration
 {
-    public static class ConfigurationDeserializer
+    public static class DataConfigurationDeserializer
     {
         public static List<ConfigurationPgn> GetConfigurationFromFile(string filePath)
         {
             if (!File.Exists(filePath))
-                throw new Exception($"IECA configuration file doesn't exist: {filePath}");
+                throw new Exception($"IECA Data configuration file doesn't exist: {filePath}");
 
             return JsonConvert.DeserializeObject<List<ConfigurationPgn>>(File.ReadAllText(filePath));
         }
