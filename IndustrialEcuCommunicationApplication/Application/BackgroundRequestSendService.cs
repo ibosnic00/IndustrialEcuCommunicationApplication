aa8dfc2f9ddbcb529f633ddba47c556cf1eac7fe
Ivan Bosnic <ivan.bosnic.st@gmail.com>
2021-01-29 14:11:49 +0100

    Refactored application, so all logic is separated to Application class

--- IndustrialEcuCommunicationApplication/Application/BackgroundRequestSendService.cs
+++ IndustrialEcuCommunicationApplication/Application/BackgroundRequestSendService.cs
@@ -1,43 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using IECA.CANBus;
 using IECA.J1939.Configuration;
+using IECA.J1939.Services;
 using IECA.J1939.Utility;
 using Microsoft.Extensions.Hosting;
 
-namespace IECA.J1939.Services
+namespace IECA.Application
 {
     internal class BackgroundRequestSendService : IHostedService, IDisposable
     {
-        private List<Timer>? _timers;
+        private readonly List<Timer>? _timers;
         private bool _disposed;
         private readonly List<ConfigurationRequestPgn> _requestPgns;
-        private readonly byte _sourceAddress;
-        private ICanInterface _canInterface;
+        private readonly IndustrialEcuCommunciationApp _app;
 
-        public BackgroundRequestSendService(List<ConfigurationRequestPgn> requestPgns, ICanInterface canInterface, byte sourceAddress)
+        public BackgroundRequestSendService(List<ConfigurationRequestPgn> requestPgns, IndustrialEcuCommunciationApp app)
         {
-            _requestPgns = requestPgns;
-            _canInterface = canInterface;
-            _sourceAddress = sourceAddress;
             _timers = new List<Timer>();
+            _requestPgns = requestPgns;
+            _app = app;
         }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
             foreach (var requestPgn in _requestPgns)
+            {
                 _timers!.Add(new Timer(x =>
                {
-                   var j1939MessageForSending = ConnectionProcedures.SendRequestForPgnMessage(requestPgn.ParameterGroupNumber, _sourceAddress);
+                   var j1939MessageForSending = ConnectionProcedures.SendRequestForPgnMessage(requestPgn.ParameterGroupNumber, _app.ClaimedAddress);
 
-                   if (j1939MessageForSending?.Data != null)
-                       _canInterface?.SendCanMessage(Helpers.ConvertSingleFrameJ1939MsgToCanMsg(j1939MessageForSending));
+                   if (j1939MessageForSending?.Data != null && _app.AddressClaimSuccessfull)
+                   {
+                       _app.CanInterface?.SendCanMessage(Helpers.ConvertSingleFrameJ1939MsgToCanMsg(j1939MessageForSending));
+                       Console.WriteLine("SentMessage " + requestPgn.ParameterGroupNumber.ToString());
+                   }
 
                }, null, dueTime: 0, requestPgn.RequestRateMilliseconds));
 
+                // HACK: SocketCAN is not able to send large amount of data in a row, 
+                // this can be removed for other can devices
+                Thread.Sleep(50);
+            }
             return Task.CompletedTask;
         }
 
