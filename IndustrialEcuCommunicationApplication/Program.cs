c6bb67c1e2bd1a8b65eb83341509810aa7e79acf
Ivan Bosnic <ivan.bosnic.st@gmail.com>
2021-02-04 18:29:53 +0100

    Added command line parser

--- IndustrialEcuCommunicationApplication/Program.cs
+++ IndustrialEcuCommunicationApplication/Program.cs
@@ -1,18 +1,31 @@
-﻿using IECA.Application;
+﻿using CommandLine;
+using IECA.Application;
 using IECA.CANBus;
 using IECA.Logging;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace IECA
 {
+    public class CommandLineOptions
+    {
+        [Option('c', "canChannel", Required = true, HelpText = "Provide CAN channel which will be used. Only one allowed from list: can0, can1, can2, can3")]
+        public CanChannel SelectedCanChannel { get; set; }
+
+        [Option('f', "appCfgFile", Required = true, HelpText = "Provide path to app configuration file (.json) which will be parsed. Eg. \"home\\pi\\someFile.json\"")]
+        public string TomlFilePath { get; set; } = null!;
+    }
+
     class Program
     {
-        private const string APP_CFG_PATH = @"/home/pi/Desktop/ieca_app_configuration.json";
-
         static void Main(string[] args)
         {
-            var mainApp = new IndustrialEcuCommunciationApp(canInterface: new SocketCanInterface(CanChannel.can0),
-                                                            appConfigurationPath: APP_CFG_PATH,
+            CommandLineOptions cmdLineOptions = null!;
+            _ = Parser.Default.ParseArguments<CommandLineOptions>(args)
+                .WithNotParsed(HandleParseError)
+                .WithParsed(parsedOptions => cmdLineOptions = parsedOptions);
+
+            var mainApp = new IndustrialEcuCommunciationApp(canInterface: new SocketCanInterface(cmdLineOptions.SelectedCanChannel),
+                                                            appConfigurationPath: cmdLineOptions.TomlFilePath,
                                                             logger: new SerilogLogger());
             mainApp.Initialize();
 
@@ -20,6 +33,12 @@ namespace IECA
             {
                 // running until user explicitly stops process
             };
+        }
+
+        //in case of errors or --help or --version
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+            System.Console.WriteLine("Unable to start application. Check command line arguments");
         }
     }
 
